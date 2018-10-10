56db2234df4cd86de11f29544144b078a763a0db
Patricia bellardine <patriciabellardine@Patricias-MacBook-Pro.local>
2018-10-10 13:34:55 -0700

    initialize repo

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require("express"); // did double quotes change after saving?
+const app = express();
+
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
