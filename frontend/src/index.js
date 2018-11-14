3e5c3530b57dfc9dba79f0d374d3e3e29de9f7ea
Patricia bellardine <patriciabellardine@Patricias-MacBook-Pro.local>
2018-11-14 14:11:20 -0800

    Add client side apollo server

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
+//npm packages
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+//styles
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// custom components
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
