4276ca350981ed86aa4f4e8d9d69c87e329dfd50
Patricia bellardine <patriciabellardine@Patricias-MacBook-Pro.local>
2018-11-14 15:56:50 -0800

    Addd client side routs

---
+++ frontend/src/components/Signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Signin extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Signin</h1>
+      </div>
+    );
+  }
+}
+
+export default Signin;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+
 //styles
 import './index.css';
 // custom components
 import App from './components/App';
+import Landing from './components/Landing';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4444/graphql',
 });
 
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/signin" component={Signin} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/Landing" component={Landing} />
+
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+);
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Root />
   </ApolloProvider>,
   document.getElementById('root')
 );
