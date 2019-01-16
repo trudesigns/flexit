55980e56a5533b98d47a805d21bfe30079121d42
Trish <trishbellardine@gmail.com>
2019-01-16 14:24:30 -0800

    Install Express Server

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config({ path: 'variables.env' });
-
-// bring in graphql middleware
-const { ApolloServer } = require('apollo-server-express');
-
-// graphql
-const { typeDefs } = require('./schema');
-const { resolvers } = require('./resolvers');
-
-//models
-const Exercise = require('./models/Exercise');
-const User = require('./models/User');
-
-// connect to db
-mongoose
-  .connect(
-    process.env.MONGO_URI,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log('DB connected'))
-  .catch(err => {
-    console.log('Error on start: ' + err.stack);
-    process.exit(1);
-  });
-
-mongoose.set('useCreateIndex', true);
-
+const express = require("express"); // did double quotes change after saving?
 const app = express();
-
-// create apollo server
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => ({ Exercise, User }),
-});
-
-server.applyMiddleware({ app });
 
 const PORT = process.env.PORT || 4444;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`);
+  console.log(`My Server listening on PORT ${PORT}`);
 });
+
+
+
