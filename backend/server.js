4aada17b0f0fece22a225cd434108f7bdeca7384
Trish <trishbellardine@gmail.com>
2019-01-16 16:26:54 -0800

    add schema

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
-const express = require("express"); // did double quotes change after saving?
-const app = express();
+const express = require('express'); // did double quotes change after saving?
+const mongoose = require('mongoose');
+require('dotenv').config({ path: './variables.env' });
+
+// models
+const Exercise = require('./models/Exercise');
+const User = require('./models/User');
 
 const PORT = process.env.PORT || 4444;
+
+//connect to db (add these lines)
+mongoose
+  .connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('DB connected'))
+  .catch(err => console.error(err));
+
+const app = express();
 
 app.listen(PORT, () => {
   console.log(`My Server listening on PORT ${PORT}`);
 });
-
-
-
