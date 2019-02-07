5fe40d95de11e9cc577cf7aea97937a642789c3f
Trish <trishbellardine@gmail.com>
2019-02-06 16:56:49 -0800

    we stopped on state notes @ testin browser

--- frontend/src/components/auth/SignupAuth.js
+++ frontend/src/components/auth/SignupAuth.js
@@ -1,22 +1,68 @@
 import React, { Component } from 'react';
 
 class SignupAuth extends Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    console.log(name, ':', value);
+  };
+
   render() {
     return (
       <div className="App">
         <h2 className="App">SignupAuth</h2>
         <form className="form">
-          <input type="text" name="username" placeholder="Username" />
-          <input type="email" name="email" placeholder="Email Address" />
-          <input type="password" name="password" placeholder="Password" />
-          <input
-            type="password"
-            name="passwordConfirmation"
-            placeholder="Confirm Password"
-          />
-          <button type="submit" className="button-primary">
-            Submit
-          </button>
+          <label htmlFor="username">
+            Username
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="email">
+            Email
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="passwordConfirmation">
+            Confirm Password
+            <input
+              type="password"
+              name="passwordConfirmation"
+              id="passwordConfirmation"
+              placeholder="Confirm Password"
+              onChange={this.handleChange}
+            />
+          </label>
+          <div>
+            <button type="button" className="button-primary">
+              Signup
+            </button>
+          </div>
         </form>
       </div>
     );
