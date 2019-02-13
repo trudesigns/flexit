a4de39054ffb5247a0b02abec7473b5e20f43443
Trish <trishbellardine@gmail.com>
2019-02-13 15:12:55 -0800

    add mustation signup

--- frontend/src/components/auth/SignupAuth.js
+++ frontend/src/components/auth/SignupAuth.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+import { Mutation } from "react-apollo";
+import { SIGNUP_USER } from "../../queries/index.js";
 class SignupAuth extends Component {
   state = {
     username: "",
@@ -21,58 +22,65 @@ class SignupAuth extends Component {
     return (
       <div className="App">
         <h2 className="App">SignupAuth</h2>
-        <form className="form">
-          <label htmlFor="username">
-            Username
-            <input
-              type="text"
-              name="username"
-              id="username"
-              placeholder="Username"
-              onChange={this.handleChange}
-              value={username}
-            />
-            />
-          </label>
-          <label htmlFor="email">
-            Email
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Email"
-              onChange={this.handleChange}
-              value={email}
-            />
-          </label>
-          <label htmlFor="password">
-            Password
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Password"
-              onChange={this.handleChange}
-              value={password}
-            />
-          </label>
-          <label htmlFor="passwordConfirmation">
-            Confirm Password
-            <input
-              type="password"
-              name="passwordConfirmation"
-              id="passwordConfirmation"
-              placeholder="Confirm Password"
-              onChange={this.handleChange}
-              value={passwordConfirmation}
-            />
-          </label>
-          <div>
-            <button type="button" className="button-primary">
-              Signup
-            </button>
-          </div>
-        </form>
+
+        <Mutation mutation={SIGNUP_USER}>
+          {() => {
+            return (
+              <form className="form">
+                <label htmlFor="username">
+                  Username
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Username"
+                    onChange={this.handleChange}
+                    value={username}
+                  />
+                  />
+                </label>
+                <label htmlFor="email">
+                  Email
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                    onChange={this.handleChange}
+                    value={email}
+                  />
+                </label>
+                <label htmlFor="password">
+                  Password
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    onChange={this.handleChange}
+                    value={password}
+                  />
+                </label>
+                <label htmlFor="passwordConfirmation">
+                  Confirm Password
+                  <input
+                    type="password"
+                    name="passwordConfirmation"
+                    id="passwordConfirmation"
+                    placeholder="Confirm Password"
+                    onChange={this.handleChange}
+                    value={passwordConfirmation}
+                  />
+                </label>
+                <div>
+                  <button type="button" className="button-primary">
+                    Signup
+                  </button>
+                </div>
+              </form>
+            );
+          }}
+        </Mutation>
       </div>
     );
   }
--- frontend/src/queries/index.js
+++ frontend/src/queries/index.js
@@ -1,11 +1,28 @@
-import { gql } from 'apollo-boost';
-
+import { gql } from "apollo-boost";
+//Exercise Queries
 export const GET_ALL_EXERCISES = gql`
   query {
     getAllExercises {
       exerciseName
       likes
       createdDate
+    }
+  }
+`;
+
+// Exercise Mutations
+
+// User Queries
+
+// User Mutations
+export const SIGNUP_USER = gql`
+  mutation SIGNUP_USER(
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    signupUser(username: $username, email: $email, password: $password) {
+      token
     }
   }
 `;
